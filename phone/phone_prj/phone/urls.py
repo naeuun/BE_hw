ecd15c0cde377f4757633e18082b33127c1ad0c3
naeun <1210naeun@gmail.com>
2025-04-03 16:09:10 +0900

    [0403] STUDY  TEST

--- phone/phone_prj/phone/urls.py
+++ phone/phone_prj/phone/urls.py
@@ -6,4 +6,8 @@ app_name = 'phone'
 urlpatterns = [
     path('', list, name = 'list'),
     path('result/', result, name = 'result'),
+    path('create/', create, name='create'),
+    path('detail/<int:id>/', detail, name='detail'),
+    path('update/<int:id>/', update, name='update'),
+    path('delete/<int:id>/', delete, name='delete'),
 ]
--- phone/phone_prj/phone/views.py
+++ phone/phone_prj/phone/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Phone
 
 def list(request):
@@ -16,3 +16,43 @@ def result(request):
     }
     
     return render(request, 'phone/result.html', context)
+
+def create(request):
+    if request.method == "POST":
+        name=request.POST.get('name')
+        phone_num=request.POST.get('phone_num')
+        email=request.POST.get('email')
+        
+        phone=Phone.objects.create(
+            name=name,
+            phone_num=phone_num, 
+            email=email
+        )
+        return redirect('phone:list')
+    return render(request, 'phone/create.html')
+
+def detail(request, id):
+    phone = get_object_or_404(Phone, id=id)
+    return render(request, 'phone/detail.html', {'phone' : phone})
+
+def update(request, id):
+    phone= get_object_or_404(Phone, id=id)
+    
+    if request.method=="POST":
+        phone.name=request.POST.get('name')
+        phone.phone_num=request.POST.get('phone_num')
+        phone.email=request.POST.get('email')
+        phone.save()
+        return redirect('phone:detail', id)
+    
+    return render(request, 'phone/update.html', {'phone':phone})
+
+
+def delete(request, id):
+    phone= get_object_or_404(Phone, id=id)
+    
+    if request.method=="POST":
+        phone.delete()
+        return redirect('phone:list')
+    
+    return render(request, 'phone/delete.html', {'phone':phone})
